df851047ccd5fa549da179fa6af9e51452c67351
Johan Vikman Lundström (jvikman) <jvikman@cisco.com>
2019-10-24 07:59:14 +0200

    Hightlight lines

--- personal/preload/personal.el
+++ personal/preload/personal.el
@@ -25,7 +25,7 @@
                    (interactive)
                    (windmove-emacs-or-tmux "left"  "tmux select-pane -L")))
 
-;; Disable highlight current line mode
+;; highlight current line mode
 (global-hl-line-mode 1)
 (set-face-background 'hl-line "#3e4446")
 (set-face-foreground 'highlight nil)
